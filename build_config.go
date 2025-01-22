e91f6cb84a28ba657dbb00bd18cb9f982a9efb01
LittleSheep <littlesheep.code@hotmail.com>
2025-01-22 17:07:14 +0800

    Get ready for build

--- build_config.go
+++ build_config.go
@@ -2,13 +2,13 @@ package main
 
 // Build constants
 
-const VERSION = "2.1.1"
+const VERSION = "2.2.1"
 
-const REPOSITORY_URL = "https://github.com/unmojang/drasl"
+const REPOSITORY_URL = "https://github.com/littlesheep2code/drasl"
 
 const LICENSE = "GPLv3"
 const LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.en.html"
 
-const DEFAULT_DATA_DIRECTORY = "drasl/data"
-const DEFAULT_STATE_DIRECTORY = "drasl/state"
-const DEFAULT_CONFIG_DIRECTORY = "" // TODO Rollback this change
+const DEFAULT_DATA_DIRECTORY = "/usr/share/drasl"
+const DEFAULT_STATE_DIRECTORY = "/var/lib/drasl"
+const DEFAULT_CONFIG_DIRECTORY = "/etc/drasl"
